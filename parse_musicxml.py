7d2e19fa515e6e0dd80d3870e65a94e3d340b8b1
Alex Yang <zixuan.yang2018@gmail.com>
2026-07-23 08:40:31 -0400

    Sort Musical Entities Dictionry

--- parse_musicxml.py
+++ parse_musicxml.py
@@ -13,14 +13,15 @@ def parse_musicxml(file_path: str):
 
     score = converter.parse(path)
 
-    right_hand = score.parts[0]
-    left_hand = score.parts[1]
-
+    # musical_entities will be a list containing a dictionary of all the notes in the score
     musical_entities = []
 
+    # Go through each of the score's parts
+    # The right hand will have part_index 0, and the left hand part_index 1
     for part_index, part in enumerate(score.parts):
         for element in part.recurse().notesAndRests:
 
+            # If it is a note, add a specific dictionary into musical_entities list
             if isinstance(element, note.Note):
                 entity = {
                     "type": "note",
@@ -32,6 +33,7 @@ def parse_musicxml(file_path: str):
 
                 musical_entities.append(entity)
 
+            # If it is a chord, do the same
             elif isinstance(element, chord.Chord):
                 entity = {
                     "type": "chord",
@@ -46,6 +48,7 @@ def parse_musicxml(file_path: str):
 
                 musical_entities.append(entity)
 
+            # If it is a rest, do the same
             elif isinstance(element, note.Rest):
                 entity = {
                     "type": "rest",
@@ -56,6 +59,16 @@ def parse_musicxml(file_path: str):
                 }
 
                 musical_entities.append(entity)
+
+    # Sort the musical notes by their offset time (instead of listing all RH notes and then all LH notes, list them according to their time position)
+    musical_entities.sort(
+        key=lambda entity: {
+            entity["offset"],
+            entity["part"]
+        }
+    )
+
+
     return musical_entities
 
 
