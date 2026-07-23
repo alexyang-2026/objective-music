07b418a4a5cbf8a518985fd50a20b0489a46bd46
Alex Yang <zixuan.yang2018@gmail.com>
2026-07-23 00:43:38 -0400

    Add loop to parse MusicXML into musical entities

--- parse_musicxml.py
+++ parse_musicxml.py
@@ -16,8 +16,54 @@ def parse_musicxml(file_path: str):
     right_hand = score.parts[0]
     left_hand = score.parts[1]
 
+    musical_entities = []
 
-parse_musicxml("/Users/alexyang/Desktop/objective-music-analysis/data/scores/liszt_transcendental_1.mxl")
+    for part_index, part in enumerate(score.parts):
+        for element in part.recurse().notesAndRests:
+
+            if isinstance(element, note.Note):
+                entity = {
+                    "type": "note",
+                    "pitches": [element.pitch.midi],
+                    "offset": float(element.getOffsetInHierarchy(part)),
+                    "duration": float(element.quarterLength),
+                    "part": part_index
+                }
+
+                musical_entities.append(entity)
+
+            elif isinstance(element, chord.Chord):
+                entity = {
+                    "type": "chord",
+                    "pitches": [
+                        chord_note.pitch.midi
+                        for chord_note in element.notes
+                    ],
+                    "offset": float(element.getOffsetInHierarchy(part)),
+                    "duration": float(element.quarterLength),
+                    "part": part_index
+                }
+
+                musical_entities.append(entity)
+
+            elif isinstance(element, note.Rest):
+                entity = {
+                    "type": "rest",
+                    "pitches": [],
+                    "offset": float(element.getOffsetInHierarchy(part)),
+                    "duration": float(element.quarterLength),
+                    "part": part_index
+                }
+
+                musical_entities.append(entity)
+    return musical_entities
+
+
+# Test — DELETE LATER
+entities = parse_musicxml("/Users/alexyang/Desktop/objective-music-analysis/data/scores/liszt_transcendental_1.mxl")
+
+for entity in entities[:50]:
+    print(entity)
 
     
 
